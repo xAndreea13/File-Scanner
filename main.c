ca5d8745695a8a3757dae7661f5eca4c55a7f36b
Alina <andreeacaragea95@gmail.com>
2024-07-01 19:10:09 +0300

    Implemented list_content function

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
 
 int main()
 {
     FILE *fileIn;
-
     char buffer[100];
 
     fileIn = fopen("input.txt", "r");
@@ -18,4 +19,23 @@ int main()
         printf("%s", buffer);
     }
 
+    char * path = "F:\\Proiecte personale";
+    list_content(path);
+
+    return 0;
+}
+
+void list_content(char * path)
+{
+    DIR * d;
+    struct dirent *dir;
+    d = opendir(path);
+
+    if(d != NULL)
+    {
+        while ((dir = readdir(d)) != NULL)
+            printf("%s\n", dir->d_name);
+    }
+
+    closedir(d);
 }
